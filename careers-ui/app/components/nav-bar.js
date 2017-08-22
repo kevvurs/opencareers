6e734cee1abd83ad2e47429d67b7b305001a6efc
Kevin Caravaggio <kevvurs@Galago686>
2017-08-21 23:38:14 -0700

    customizing class elements

--- careers-ui/app/components/nav-bar.js
+++ careers-ui/app/components/nav-bar.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   userActivity: Ember.inject.service(),
 
+  tagName: 'nav',
+  classNames: ['navbar', 'fixed-top', 'navbar-expand', 'op-nav'],
   signed: Ember.computed.alias('userActivity.loggedIn'),
   showSignDlg: false,
 
